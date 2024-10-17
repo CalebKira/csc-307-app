278bb09d723f7fca3837f1e79c1977ade888955a
Caleb Kira <ckira@calpoly.edu>
2024-10-16 23:01:03 -0700

    ID part 2

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -22,6 +22,20 @@ function MyApp() {
       });
   }, []);
 
+  // src/MyApp.js (a new inner function inside MyApp())
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+
+    return promise;
+  }
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -30,7 +44,16 @@ function MyApp() {
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(response => {
+        if (response.status === 201) {
+          // Do something if the status code is 200 (OK)
+          setCharacters([...characters, person])
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
 
